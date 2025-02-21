7f26e867a61923e9b3ce1e4098e2a0582ac09729
Ty Steinbach <tysteinbach@gmail.com>
2025-02-21 00:08:13 -0600

    direct API test

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,7 +1,7 @@
 export async function fetchStockSearch(ticker) {
     try {
         //Fetch data from API
-        const response = await fetch(`/api/fetch-stock-search.js?ticker=${ticker}`);
+        const response = await fetch(`https://api.polygon.io/v3/reference/tickers?ticker.gte=${ticker}&active=true&limit=5&apiKey=yfaBNM03NJpy3uNeuui5xCCRrgDqtbTd`);
         
         //If response is not ok, throw error
         if (response.status == 500) {
