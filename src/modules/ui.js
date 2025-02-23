ff1274428edabcc4b10bfd7c395714e9a185bc4a
Ty Steinbach <tysteinbach@gmail.com>
2025-02-22 19:24:19 -0600

    More bug fixes

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -7,7 +7,7 @@ export async function displayGainersLosers(data, direction) {
     const container = document.getElementById(direction);
 
     const Event = await loadEvent();
-
+    
     var symbol;
     var operator;
 
@@ -115,38 +115,47 @@ export function autocomplete(inp, arr, arr1) {
     the text field element and an array of possible autocompleted values:*/
     var currentFocus;
     /*execute a function when someone writes in the text field:*/
-
     var a, b, i, val = inp.value;
     /*close any already open lists of autocompleted values*/
     closeAllLists();
     if (!val) { return false;}
     currentFocus = -1;
+
     /*create a DIV element that will contain the items (values):*/
     a = document.createElement("DIV");
     a.setAttribute("id", inp.id + "autocomplete-list");
     a.setAttribute("class", "autocomplete-items");
+
     /*append the DIV element as a child of the autocomplete container:*/
     document.getElementById('results').appendChild(a);
+
     /*for each item in the array...*/
     for (i = 0; i < arr.length; i++) {
         /*check if the item starts with the same letters as the text field value:*/
         if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-        /*create a DIV element for each matching element:*/
-        b = document.createElement("DIV");
-        /*make the matching letters bold:*/
-        b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-        b.innerHTML += arr[i].substr(val.length) + "  (" + arr1[i] + ")";
-        /*insert a input field that will hold the current array item's value:*/
-        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-        /*execute a function when someone clicks on the item value (DIV element):*/
-            b.addEventListener("click", function(e) {
-            /*insert the value for the autocomplete text field:*/
-            inp.value = this.getElementsByTagName("input")[0].value;
-            /*close the list of autocompleted values,
-            (or any other open lists of autocompleted values:*/
-            closeAllLists();
-        });
-        a.appendChild(b);
+            /*create a DIV element for each matching element:*/
+            b = document.createElement("DIV");
+
+            /*make the matching letters bold:*/
+            b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+            b.innerHTML += arr[i].substr(val.length) + "  (" + arr1[i] + ")";
+            
+            /*insert a input field that will hold the current array item's value:*/
+            b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+
+            /*execute a function when someone clicks on the item value (DIV element):*/
+            b.addEventListener("click", async function(e) {
+                const Event = await loadEvent();
+                /*insert the value for the autocomplete text field:*/
+                inp.value = this.getElementsByTagName("input")[0].value;
+
+                /*close the list of autocompleted values,
+                (or any other open lists of autocompleted values:*/
+                closeAllLists();
+
+                Event.clickStockSearch();
+            });
+            a.appendChild(b);
         }
     }
  
@@ -202,10 +211,7 @@ export function autocomplete(inp, arr, arr1) {
     }
     }
     /*execute a function when someone clicks in the document:*/
-    document.addEventListener("click", async function (e) {
-        const Event = await loadEvent();
+    document.addEventListener("click", function (e) {
         closeAllLists(e.target);
-
-        Event.clickStockSearch();
     });
 }
