83d2b89841c9d2a925769e33422a352eb4b35a07
Ty Steinbach <tysteinbach@gmail.com>
2025-02-24 16:56:29 -0600

    Bug fix

--- src/app-login.js
+++ src/app-login.js
@@ -2,10 +2,12 @@
  * Loads JS file in code split format
  * @returns {Object} - Object storing exported data.
  */
-async function Event() {
+async function loadEvent() {
     const Events = await import("./modules/event.js");
     return Events;
 }
+
+const Events = await loadEvent();
 
 //Upon clicking submit button, attempt logging in
 document.getElementById('submit').addEventListener('click',Events.loginEvent);
